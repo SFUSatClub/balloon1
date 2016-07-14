777db92624c1ba892d15c5f46be26d46beb5fed5
richarthurs <Rich@Richards-Mac-Pro.local>
2016-07-13 23:02:00 -0700

    Investigating weird timing issue

--- lib/richard/task1.cpp
+++ lib/richard/task1.cpp
@@ -3,7 +3,9 @@
 static int trigger = 0;
 
 void task1(void){
-  if(trigger == 1){ 
+  cli();//disable interrupts
+
+  if(trigger == 1){
     digitalWrite(13, 1);
     trigger = 0;
   }
@@ -11,4 +13,5 @@ void task1(void){
     digitalWrite(13, 0);
     trigger = 1;
   }
+  sei();
 }
