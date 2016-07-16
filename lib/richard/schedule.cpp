a258c911c66c6508d362f045094a7bd5fc8448ab
richarthurs <Rich@Richards-Mac-Pro.local>
2016-07-16 11:11:20 -0700

    Revert ec98654..2e71bd9
    This rolls back to commit ec9865419814c01374897ed76cebce304d37af75.

--- lib/richard/schedule.cpp
+++ lib/richard/schedule.cpp
@@ -1,22 +1,22 @@
 #include "schedule.h"
 #include "tasks.h"
 
-Task::Task(uint16_t Interval){
-  // timeOut = timeout;
+Task::Task(uint16_t timeout, uint16_t Interval){
+  timeOut = timeout;
   lastRun = 0;
-  // runStart = 0;
+  runStart = 0;
   interval = Interval;
   pointedFunc =  nullptr; // pointedFunc(nullptr)
 }
 
 
-void Task::setLastRun(uint32_t lastrun){
+void Task::setLastRun(uint16_t lastrun){
   lastRun = lastrun;
 }
 
-//  void Task::setRunStart(uint16_t runstart){
-//   runStart = runstart;
-// }
+ void Task::setRunStart(uint16_t runstart){
+  runStart = runstart;
+}
 
 
 void Task::setFunc( funcPtr f ) {
--- lib/richard/schedule.h
+++ lib/richard/schedule.h
@@ -2,20 +2,19 @@
 
 class Task{
 private:
-  //  uint16_t timeOut;
-  //  uint16_t runStart;
+   uint16_t timeOut;
+   uint16_t runStart;
   typedef void (*funcPtr)(void); // create a type to contain the function pointer
   funcPtr pointedFunc; // pointedFunc is the variable with the function pointer
 
 public:
   // we need to poke at these as we're going through checking whether to run tasks or not, hence public
-   uint32_t lastRun;
+    uint16_t lastRun;
    uint16_t interval;
 
-  // Task(); // default constructor
-  Task(uint16_t Interval); // constructor
-  void setLastRun(uint32_t lastrun);
-// void setRunStart(uint16_t runstart);
+  Task(uint16_t timeout, uint16_t Interval); // constructor
+ void setLastRun(uint16_t lastrun);
+void setRunStart(uint16_t runstart);
   void runTask();
   void setFunc(funcPtr f);
 };
