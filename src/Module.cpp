f795104780b0a1aeffb47dcd84a79990de1ac58b
Steven Huang <stevenhuang1995@gmail.com>
2016-07-19 03:43:17 -0700

    Relax pure virtual constraint of enable() and disable() when deriving Modules
    - Modules now aren't forced to implement enable()/disable() if there's no reason to

--- src/Module.cpp
+++ src/Module.cpp
@@ -3,7 +3,7 @@
 
 // Steven: Bitwise operators to use State enum as flags
 inline Module::State operator|(Module::State a, Module::State b) {
-	return static_cast<Module::State>(static_cast<int>(a) | static_cast<int>(b));
+    return static_cast<Module::State>(static_cast<int>(a) | static_cast<int>(b));
 }
 inline Module::State operator &(Module::State a, Module::State b) {
     return static_cast<Module::State>(static_cast<int>(a) & static_cast<int>(b));
@@ -13,15 +13,23 @@ inline Module::State& operator |=(Module::State& a, Module::State b) {
 }
 
 Module::Module() {
-	// Steven: nothing for now
+    // Steven: nothing for now
+}
+
+int Module::enable() {
+    return 0;
+}
+
+void Module::disable() {
+    return;
 }
 
 const char* Module::dataToPersist() {
-	return NULL;
+    return NULL;
 }
 
 const char* Module::getModuleName() {
-	return "Module";
+    return "Module";
 }
 
 // Serial streams
--- src/Module.h
+++ src/Module.h
@@ -16,18 +16,20 @@ public:
 		GOOD = 1 << 2
 	};
 	
-	// Steven: Pure virtual functions, they must be implemented in derived classes
-	// otherwise will get linker error 'undefined reference to vtable'. Forces
-	// all sensor 'modules' to have a common interface.
-	
+	// Steven: Pure virtual functions: begin() + tick(), must be
+	// implemented in derived classes otherwise will get linker error
+	// 'undefined reference to vtable'. Forces all sensor 'modules' to have a
+	// common interface.
+
 	// Called once in setup()
 	virtual void begin() = 0;
 	// Called periodically in loop()
 	virtual void tick() = 0;
+
 	// Enable the module, renable power
-	virtual int enable() = 0;
+	virtual int enable();
 	// Disable the module, powersaving mode
-	virtual void disable() = 0;
+	virtual void disable();
 	// Should the module creates data and wants to persist it, this function
 	// will be called by the SD module to take care of it
 	virtual const char* dataToPersist();
