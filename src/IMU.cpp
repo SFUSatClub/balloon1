4ae8761e9a1c1319f3bc65b741ba761b5735579d
Lana <borklana@gmail.com>
2016-07-23 13:44:12 -0700

    Added IMU Module

--- src/IMU.cpp
+++ src/IMU.cpp
@@ -1,24 +1,61 @@
 #include "IMU.h"
 
-IMU::IMU(int _i2cAddr)
-	: i2cAddr(_i2cAddr)
-{
-
+IMU::IMU() {
+  imuImpl = new LSM303;
 }
 
-void IMU::begin(){
-
+void IMU::begin() {
+  Serial.begin(9600);
+  Wire.begin();
+  Serial.print("Begin IMU\n");
+  imuImpl->init();
+  imuImpl->enableDefault();
 }
 
 void IMU::tick(){
-
+  char response[80];
+  Serial.print("Tick IMU\n");
+  imuImpl->read();
+  dataAccelerometer[0] = imuImpl->a.x;
+  dataAccelerometer[1] = imuImpl->a.y;
+  dataAccelerometer[2] = imuImpl->a.z;
+  dataMagnetometer[0] = imuImpl->a.x;
+  dataMagnetometer[1] = imuImpl->a.y;
+  dataMagnetometer[2] = imuImpl->a.z;
+  snprintf(response, sizeof(response), "A: %6d %6d %6d    M: %6d %6d %6d",
+    imuImpl->a.x, imuImpl->a.y, imuImpl->a.z,
+    imuImpl->m.x, imuImpl->m.y, imuImpl->m.z);
+  Serial.println(response);
+  delay(100);
 }
 
-const char* IMU::dataToPersist(){
-	return NULL;
+// Data in format <accX>,<accY>,<accZ>,<magX>,<magY>,<magZ>
+const char* IMU::dataToPersist() {
+  toWrite[0] = '\0';
+	for (int i=0;i<3;i++){
+		sprintf(strchr(toWrite,'\0'), "%d,", dataAccelerometer[i]); //appends to output str
+	}
+  for (int i=0;i<3;i++){
+    sprintf(strchr(toWrite,'\0'), "%d,", dataMagnetometer[i]); //appends to output str
+  }
+	return toWrite;
 }
 
-const char* IMU::getModuleName() {
-	return "IMU";
+int IMU::getAccX() {
+  return dataAccelerometer[0];
 }
-
+int IMU::getAccY() {
+  return dataAccelerometer[1];
+}
+int IMU::getAccZ() {
+  return dataAccelerometer[2];
+}
+int IMU::getMagX() {
+  return dataMagnetometer[0];
+}
+int IMU::getMagY() {
+  return dataMagnetometer[1];
+}
+int IMU::getMagZ() {
+  return dataMagnetometer[2];
+}
--- src/IMU.h
+++ src/IMU.h
@@ -1,17 +1,32 @@
 #ifndef IMU_H
 #define IMU_H
 
+#include <Wire.h>
+#include <LSM303.h>
 #include "Module.h"
 
-class IMU: public Module { 
-	const int i2cAddr;
+class IMU: public Module {
+  LSM303 *imuImpl;
+	uint32_t timer;
+  int dataAccelerometer[3];
+  int dataMagnetometer[3];
+  char toWrite[200];
 public:
-	IMU(int i2cAddr);
+	IMU();
 	void begin();
 	void tick();
+	int enable();
+	void disable();
 
 	const char* dataToPersist();
 	const char* getModuleName();
+
+  int getAccX();
+  int getAccY();
+  int getAccZ();
+  int getMagX();
+  int getMagY();
+  int getMagZ();
 };
 
-#endif // IMU_H
+#endif
