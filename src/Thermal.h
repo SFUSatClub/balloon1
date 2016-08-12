d674232477e5335c9fc8b989f6f05c8afaa1b44b
Lana <borklana@gmail.com>
2016-08-11 20:28:57 -0700

    Added support for multiple termerature sensors

--- src/Thermal.h
+++ src/Thermal.h
@@ -7,16 +7,20 @@
 class Thermal: public Module {
   double currentR;
   double currentTempC;
-  char toWrite[150];
+  char toWrite[10];
   const int THERMAL_PIN;
+  // R2 in voltage divider used to caculate R1 (Thermistor R), in kOhms
+  // Resistor needs to be changed if more precision is required in different temperature ranges
+  // Ex: get a resistor with higher resistance to obtain bigger precision for negative temperatures
+  const double R2;
+  const char* name;
 public:
-  Thermal(int pin);
+  Thermal(int pin, double R, char const * id);
   void begin();
   void tick();
 
   const char* dataToPersist();
   const char* getModuleName();
-
 };
 
 #endif
